cae8ceb147618c50e365e80f3f326d760ebefecd
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-01-20 18:14:38 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>"Nunca confie em alguém que não tenha trazido um livro consigo." - Lemony Snicket</h1>;
+  return (
+    <h1>
+      "Nunca confie em alguém que não tenha trazido um livro consigo." - Lemony
+      Snicket
+    </h1>
+  );
 }
 
 export default Home;
