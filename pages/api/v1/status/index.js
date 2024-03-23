97c192942f56144cdb0f80a75f396f49360dfaf4
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-03-23 01:34:04 +0000

    feat: add more tests for status route

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,14 @@
 import database from 'infra/database';
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as SUM;');
-  console.log(result.rows);
+  // const result = await database.query('SELECT 1 + 1 as SUM;');
+  // console.log(result.rows);
 
-  response.status(200).json({ status: "OK, a API está funcionando" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET `/api/v1/status`should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toBe(responseBody.updated_at);
 });
